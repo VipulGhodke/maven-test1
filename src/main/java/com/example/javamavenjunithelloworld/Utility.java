83fde30ae51da492750a3735ec0666965c096081
VipulGhodke <vipulghodke199@gmail.com>
2024-09-17 13:39:01 +0530

    Feature/VIPULG0001- added code for finding whether number is even or odd

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,20 @@
+package com.example.javamavenjunithelloworld;
+
+public class Utility {
+    public static void oddEven(int n) {
+        if (n % 2 == 0)
+            System.out.println(n + " is Even");
+        else
+            System.out.println(n + " is Odd");
+
+    }
+
+    public static void vipul() {
+        System.out.println("I am Vipul");
+    }
+
+    public static void main(String[] args) {
+        // vipul();
+        // oddEven(10);
+    }
+}
